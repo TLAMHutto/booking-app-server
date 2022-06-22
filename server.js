bca2d7ff25a8fb739d166c7f12ea8ad620f01289
TLAMHutto <keatonhutto8@gmail.com>
2022-06-21 19:05:07 -0500

    connected db

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 import express from 'express';
-
+const morgan = require('morgan');
+import cors from 'cors';
+import mongoose from 'mongoose';
 import {readdirSync} from 'fs';
 require('dotenv').config();
 
 const app = express();
+
+
+app.use(cors());
+app.use(morgan('dev'));
 readdirSync('./routes').map((r) => app.use('/api', require(`./routes/${r}`)));
 
 const port = process.env.PORT || 8000;
 
-app.listen(`${port}`, () => {
-    console.log("Server is running on port 3000");
+mongoose
+  .connect(process.env.DATABASE, {})
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log("DB Error => ", err));
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
