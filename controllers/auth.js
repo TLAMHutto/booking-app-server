df11b8dd9a1847e420ee232e04c774b13d60e4a7
TLAMHutto <keatonhutto8@gmail.com>
2022-06-22 21:41:44 -0500

    server issuse

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,25 @@
-export const showMessage = (req, res) => {
-    res.status(200).send(`Here is your message ${req.params.message}`);
-}
+import User from '../models/user';
+
 
 export const register = async (req, res) => {
     console.log(req.body)
+    const {name, email, password} = req.body;
+
+    if(!name) return res.status(400).send('Name is required');
+    if(!email) return res.status(400).send('Email is required');
+    if(!password|| password.length< 6) return res.status(400).send("Password is too short");
+    
+    let userExist = await User.findOne({email});
+    if (userExist) return res.status(400).send('User already exists');
+    const user = new User(req.body)
+    try{
+        await user.save()
+        console.log('User saved', user)
+        res.status(201).send(user)
+        return res.json({ok: true})
+
+    }catch (err) {
+        console.log(err)
+        return res.status(400).send(err)
+    }
 }
--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ import express from 'express';
 
 
 const router = express.Router();
-import {showMessage, register} from '../controllers/auth';
-router.get("/:message", showMessage);
+import {register} from '../controllers/auth';
+
 router.post('/register', register)
 module.exports = router;
